888a2b13d1a1286e406af0b153d00fe7f0989857
abishake <sritharabishake@gmail.com>
2025-03-25 18:14:30 +0000

    Docker setup, building and running but not accessable on browser

---
+++ src/main/kotlin/FitnessTrackerController.kt
@@ -0,0 +1,13 @@
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class FitnessTrackerController {
+
+    @GetMapping("/workouts")
+    fun getWorkouts(): String {
+        return "Workouts data"
+    }
+}
