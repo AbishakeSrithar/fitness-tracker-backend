4e94bbc456d5576d8f26c5cc8e45a341451cf411
Abishake Srithar <sritharabishake@gmail.com>
2025-05-12 08:37:36 +0100

    Add start.sh script

---
+++ scripts/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Executing Start Script"
+
+containers_to_check=("kotlin_api" "postgresql")
+
+for name in "${containers_to_check[@]}"; do
+  # Get container IDs
+  container_ids=$(docker ps -a --filter "name=$name" --filter "status=exited" --format "{{.ID}}")
+
+  for id in $container_ids; do
+    echo "docker remove $id"
+    docker remove "$id"
+  done
+done
+
+cd ..
+
+echo "mvn clean install"
+mvn clean install
+
+echo "docker compose up"
+docker compose up
