d49446a560be6db29bc4855e083d7b5cb19d9a8a
Abishake Srithar <sritharabishake@gmail.com>
2025-05-27 16:49:44 +0100

    Cleanup CSV converter code

--- src/main/kotlin/com/abishake/fitnesstracker/controllers/importCSVController.kt
+++ src/main/kotlin/com/abishake/fitnesstracker/controllers/importCSVController.kt
@@ -1,6 +1,9 @@
 package com.abishake.fitnesstracker.controllers
 
+import com.abishake.fitnesstracker.models.Entry
+import com.abishake.fitnesstracker.models.RestResponse
 import com.abishake.fitnesstracker.service.CSVConverterService
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,12 +15,20 @@ import org.springframework.web.bind.annotation.RestController
 class importCSVController (
     private val csvConverterService: CSVConverterService
 ) {
+
+    private val className = EntryController::class
+
     // READ
     @GetMapping(
         value = ["/get"],
         produces = ["application/json"]
     )
-    fun getAllEntries() {
-        csvConverterService.readCSV()
+    fun importCSV(): RestResponse<List<Unit>> {
+        try {
+            csvConverterService.readCSV()
+            return RestResponse(true, "Import CSV",  emptyList())
+        } catch (e: Exception) {
+            throw Exception("Exception in importCSV() >> $className \n $e", e)
+        }
     }
 }
--- src/main/kotlin/com/abishake/fitnesstracker/service/CSVConverterService.kt
+++ src/main/kotlin/com/abishake/fitnesstracker/service/CSVConverterService.kt
@@ -11,8 +11,8 @@ class CSVConverterService (
     private val entryService: EntryService
 ) {
 
-    val createdWorkoutDates = mutableListOf<String>()
-    val createdExerciseNames = mutableListOf<String>()
+    val createdWorkoutDates = mutableMapOf<String, Long>()
+    val createdExerciseNames = mutableMapOf<String, Long>()
 
     fun readCSV() {
         var i = 0
@@ -24,97 +24,51 @@ class CSVConverterService (
             delimiter = ';'
         }.open(inputStream) {
             readAllWithHeaderAsSequence().forEach { row ->
-                println(i)
-                i++
-                if (row["Set Order"] != "Note" && row["Set Order"] != "Rest Timer") {
-                    println("Workout #: ${row["Workout #"]}, Exercise Name: ${row["Exercise Name"]}, Weight (kg): ${row["Weight (kg)"]}, Reps: ${row["Reps"]}")
-                    println("Date: ${row["Date"]}")
-
+                if (row["Set Order"] != "Note" && row["Set Order"] != "Rest Timer") { // Specific for my data
                     createAllWorkouts(row)
                     createAllExercises(row)
                     createAllEntries(row)
-
-
                 }
             }
         }
     }
 
     fun createAllWorkouts(row:  Map<String, String>) {
-        // Very reliant on 1 workout per day - will break otherwise
         val date = row["Date"]?.slice(0..9)
-        val year = Integer.parseInt(date?.slice(0..3))
-        val month = Integer.parseInt(date?.slice(5..6))
-        val day = Integer.parseInt(date?.slice(8..9))
         val workoutName = row["Workout Name"]
-        if (date != null && workoutName != null && date in createdWorkoutDates) {
-            println("Already created Workout for: ${row["Date"]}")
-        } else if (date != null  && workoutName != null && date !in createdWorkoutDates) {
-            // If workout by date does not exist, create
-            workoutService.createWorkout(workoutName, LocalDate.of(year, month, day))
-            // add to cache (can hopefully use this to skip gets/creates too much
-            createdWorkoutDates.add(date)
-            println("Creating Workout for: ${row["Date"]}")
-        } else {
-            println("Can't parse this Date: ${row["Date"]}")
+        if (date != null  && workoutName != null && date !in createdWorkoutDates.keys) {
+            val payload = workoutService.createWorkout(workoutName, LocalDate.parse(date))
+            payload[0].id?.let { createdWorkoutDates.put(workoutName, it) }
         }
     }
 
     fun createAllExercises(row:  Map<String, String>) {
-        // Very reliant on 1 workout per day - will break otherwise
-        val exercise = row["Exercise Name"]
-        if (exercise != null && exercise in createdExerciseNames) {
-            println("Already created Exercise for: ${row["Exercise Name"]}")
-        } else if (exercise != null && exercise !in createdExerciseNames) {
-            // If workout by date does not exist, create
-            exerciseService.createExercise(exercise, exercise)
-            // add to cache (can hopefully use this to skip gets/creates too much
-            createdExerciseNames.add(exercise)
-            println("Creating Exercise for: ${row["Exercise Name"]}")
-        } else {
-            println("Can't parse this Exercise: ${row["Exercise Name"]}")
+        val exerciseName = row["Exercise Name"]
+        if (exerciseName != null && exerciseName !in createdExerciseNames.keys) {
+            val payload = exerciseService.createExercise(exerciseName, exerciseName)
+            payload[0].id?.let { createdExerciseNames.put(exerciseName, it) }
         }
     }
 
     fun createAllEntries(row:  Map<String, String>) {
-        // Very reliant on 1 workout per day - will break otherwise
         val exercise = row["Exercise Name"]
         val date = row["Date"]?.slice(0..9)
-        println(row["Weight (kg)"])
         val weight = row["Weight (kg)"]
         val reps = row["Reps"]
         val seconds = row["Seconds"]
         if (exercise != null && date != null && weight != null && reps != null && seconds != null) {
-            println("inside here")
-            // should probs cache id instead of fetching everytime
-            // HEAVILY ASSUMING THERE'S ONLY 1 MATCH
             val workoutId = workoutService.getWorkoutsByDate(LocalDate.parse(date))[0].id?.toInt()
             val exerciseId = exerciseService.getExerciseByName(exercise)[0].id?.toInt()
-            var weightAsDouble: Double = 0.0
-
-            if (weight != "") {
-                weightAsDouble = weight.toDouble()
+            val weightAsDouble = weight.toDoubleOrNull() ?: 0.0
+            val repsOrSecs = when {
+                reps.isNotBlank() -> reps.toIntOrNull() ?: 0
+                seconds.isNotBlank() -> seconds.toDoubleOrNull()?.toInt() ?: 0
+                else -> 0
             }
-            var repsAsInt: Int = 0
-
-            repsAsInt = if (reps != "") {
-                reps.toInt()
-            } else if (seconds != "") {
-                println("seconds: $seconds")
-                seconds.toDouble().toInt()
-            } else {
-                0
-            }
-
-            println(workoutId)
-            println(exerciseId)
 
             if (workoutId != null && exerciseId != null) {
-                println("createEntry")
-                entryService.createEntry(workoutId, exerciseId, weightAsDouble, 1, repsAsInt)
+                entryService.createEntry(workoutId, exerciseId, weightAsDouble, 1, repsOrSecs)
             }
-
-
         }
     }
 }
