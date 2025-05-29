109d42882ebd46484f02272d0cbb104d1031b888
abishake <sritharabishake@gmail.com>
2025-05-29 08:40:25 +0100

    Update start.sh to not nuke all docker volumes

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 echo "Executing Start Script"
+# ------ CAREFUL
+# ------ Keeping this in case I need to nuke all Volumes/Containers during local but if we
+# ------ have multiple dockerized projects, we probably shouldn't use the commented out part.
 
-containers_to_check=("kotlin_api" "postgresql")
-
-for name in "${containers_to_check[@]}"; do
-  # Get container IDs
-  container_ids=$(docker ps -a --filter "name=$name" --filter "status=exited" --format "{{.ID}}")
-
-  for id in $container_ids; do
-    echo "docker remove $id"
-    docker remove "$id"
-  done
-done
-
-echo "docker volume prune -f"
-docker volume prune -f
+#containers_to_check=("kotlin_api" "postgresql")
+#
+#for name in "${containers_to_check[@]}"; do
+#  # Get container IDs
+#  container_ids=$(docker ps -a --filter "name=$name" --filter "status=exited" --format "{{.ID}}")
+#
+#  for id in $container_ids; do
+#    echo "docker remove $id"
+#    docker remove "$id"
+#  done
+#done
+#
+#echo "docker volume prune -f"
+#docker volume prune -f
 
 cd ..
 
