fcac2c170716d05f3d6e3dea3062fc38d676c7dd
Abishake Srithar <sritharabishake@gmail.com>
2025-05-30 08:27:56 +0100

    remove docker docker so that it actually removes volumes

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ echo "Executing start.sh Script"
 
 # bring down if up and also remove associated volumes (prevent db data from staying - flyways complaints when .sql changes)
 echo "docker compose down -v"
-docker docker compose down -v
+docker compose down -v
 
 echo "docker compose up --build"
 docker compose up --build
